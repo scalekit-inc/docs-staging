775b61c24b59fd669a074cad05ab72d04bcc77df
Saif Ali Shaik <saif@Saifs-MacBook-Pro.local>
2024-10-14 21:52:44 +0530

    Update navbar text responsiveness and CSS styles
    - Adjust text in the navbar based on screen size for SSO and SCIM links.
    - Add a function to update text dynamically on window resize.
    - Modify CSS styles for navbar links and other elements to use new color variables.

--- scalekit.js
+++ scalekit.js
@@ -7,20 +7,38 @@ export function onRouteDidUpdate({ location, previousLocation }) {
   var dirSyncLink = document.querySelector('a.dropdown__link.scim');
   var dropdownNavbarItem = document.querySelector('a.navbar__link');
 
+  function updateTextBasedOnScreenSize() {
+    if (window.innerWidth <= 1350 && window.innerHeight <= 1430) {
+      if (ssoLink && document.location.href.includes('/sso/')) {
+        dropdownNavbarItem.innerHTML = 'SSO';
+      } else if (dirSyncLink && document.location.href.includes('/scim/')) {
+        dropdownNavbarItem.innerHTML = 'SCIM';
+      }
+    } else {
+      if (ssoLink && document.location.href.includes('/sso/')) {
+        dropdownNavbarItem.innerHTML = 'Single Sign-On';
+      } else if (dirSyncLink && document.location.href.includes('/scim/')) {
+        dropdownNavbarItem.innerHTML = 'SCIM Provisioning';
+      }
+    }
+  }
+
   if (ssoLink) {
     if (document.location.href.includes('/sso/')) {
       ssoLink.classList.add('navbar__link--active');
       dirSyncLink.classList.remove('navbar__link--active');
-      console.log(dropdownNavbarItem.innerHTML);
-      dropdownNavbarItem.innerHTML = 'Single Sign-On';
+      updateTextBasedOnScreenSize();
     } else if (document.location.href.includes('/scim/')) {
       dirSyncLink.classList.add('navbar__link--active');
       ssoLink.classList.remove('navbar__link--active');
-      dropdownNavbarItem.innerHTML = 'SCIM Provisioning';
+      updateTextBasedOnScreenSize();
     } else {
       ssoLink.classList.remove('navbar__link--active');
       dirSyncLink.classList.remove('navbar__link--active');
       dropdownNavbarItem.innerHTML = 'Products';
     }
   }
+
+  window.addEventListener('resize', updateTextBasedOnScreenSize);
+  updateTextBasedOnScreenSize(); // Initial call to set the correct text
 }
