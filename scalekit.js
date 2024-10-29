02e4b5f7961db3ab86de0dca688852b3e2092daf
Ravi Madabhushi <ravi.madabhushi@scalekit.com>
2024-10-29 12:17:32 +0530

    adding simple sso quickstart guide for in-product embedding

--- scalekit.js
+++ scalekit.js
@@ -4,7 +4,14 @@ export function onRouteDidUpdate({ location, previousLocation }) {
     document.documentElement.dataset.insideIframe =
       window.parent !== window;
   }
-  // if (location.pathname != homepageURL) {
-  //   document.querySelector('.homeLink').classList.remove('navbar__link--active');
-  // }
+  var ssoLink = document.querySelector(
+    'a.navbar__item.navbar__link.sso',
+  );
+  if (ssoLink) {
+    if (document.location.href.includes('/sso/')) {
+      ssoLink.classList.add('navbar__link--active');
+    } else {
+      ssoLink.classList.remove('navbar__link--active');
+    }
+  }
 }
