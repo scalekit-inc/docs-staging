f422b189085fe649de0316f68e83cc80d7b24902
scalekit-devs <saif.shaik@scalekit.com>
2024-11-20 16:36:01 +0530

    staging version of scalekit.js

--- scalekit.js
+++ scalekit.js
@@ -1,16 +1,10 @@
+var homepageURL = '/docs-staging/';
 export function onRouteDidUpdate({ location, previousLocation }) {
   if (location.pathname !== previousLocation?.pathname) {
     document.documentElement.dataset.insideIframe =
-      window.parent !== window;
+      document.location.href.includes('/integrations/') && window.parent !== window;
   }
-  var ssoLink = document.querySelector(
-    'a.navbar__item.navbar__link.sso',
-  );
-  if (ssoLink) {
-    if (document.location.href.includes('/sso/')) {
-      ssoLink.classList.add('navbar__link--active');
-    } else {
-      ssoLink.classList.remove('navbar__link--active');
-    }
-  }
+  // if (location.pathname != homepageURL) {
+  //   document.querySelector('.homeLink').classList.remove('navbar__link--active');
+  // }
 }
