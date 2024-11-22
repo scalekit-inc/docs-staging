0b2e24f466cbab8381d60b2f23ffafa65b0125f3
scalekit-devs <saif.shaik@scalekit.com>
2024-11-22 16:32:58 +0530

    Fast follow v0 - (Update SCIM documentation and improve code examples) (#39)
    * Adjust hero section layout on homepage
    
    - Changed the column width of the hero text from 6 to 4
    - Increased the column width of the hero image from 6 to 7
    
    * Vertically center align navbar__inner element
    
    - Added flexbox properties to the navbar class to center its inner content vertically.
    
    * Update SCIM quickstart documentation
    
    - Clarified SCIM provisioning with Scalekit
    - Updated REST API capabilities description
    
    * Update SCIM documentation and SDK versions
    
    - Removed default organization reference in quickstart
    - Updated Java SDK version to 1.0.2
    - Adjusted API calls to include organization ID
    - Enhanced webhook documentation with new code block
    - Improved consistency in client initialization examples
    
    * set the iframe to hide it in-product-experience

--- scalekit.js
+++ scalekit.js
@@ -2,7 +2,7 @@
 export function onRouteDidUpdate({ location, previousLocation }) {
   if (location.pathname !== previousLocation?.pathname) {
     document.documentElement.dataset.insideIframe =
-      document.location.href.includes('/integrations/') && window.parent !== window;
+      window.parent !== window;
   }
   // if (location.pathname != homepageURL) {
   //   document.querySelector('.homeLink').classList.remove('navbar__link--active');
